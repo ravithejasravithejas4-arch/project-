0005b190229a701dbf1845071f9c9510b7edfb15
ravithejasravithejas4-arch <ravithejasravithejas4@gmail.com>
2026-05-08 15:36:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from gtts import gTTS
 from io import BytesIO
 
 def main():
-    st.set_page_config(page_title="PragyanAI - VVIET Multimedia Hub", layout="wide")
+    st.set_page_config(page_title="ssb 1.pdf", layout="wide")
     st.image("PragyanAI_Transperent.png")
     st.title(" PragyanAI Multi-Functional Media Hub")
     
